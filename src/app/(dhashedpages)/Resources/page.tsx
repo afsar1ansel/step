eb1c3c57947f861705c913e42f2670a28f33c897
afsar1ansel <mdafsaransal@gmail.com>
2025-02-10 16:22:32 +0530

    resources

--- src/app/(dhashedpages)/Resources/page.tsx
+++ src/app/(dhashedpages)/Resources/page.tsx
@@ -8,53 +8,93 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import type { ColDef } from "ag-grid-community";
 import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
 import styles from "./page.module.css";
-
+import { FaSearch } from "react-icons/fa";
+import Image from "next/image";
+import photo from "/public/BG.png";
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 const DataLogs = () => {
- 
-
   return (
-    <div style={{ width: "80vw", height: "60vh", maxWidth: "1250px" }}>
+    <div
+      style={{
+        width: "80vw",
+        height: "60vh",
+        maxWidth: "1250px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "0 auto",
+      }}
+    >
       <div className={styles.hello}>
-        <h3>All Recources</h3>
+        <h2>All Recources</h2>
         <p>
           Access, monitor, and manage uploaded data logs with detailed
           processing history for better transparency and control.
         </p>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <FaSearch className={styles.searchIcon} />
+          <input
+            type="text"
+            placeholder="Search..."
+            className={styles.searchinput}
+          />
+        </div>
       </div>
       <div
         style={{
           height: "100%",
           width: "80vw",
           marginTop: "40px",
+          display: "flex",
+          justifyContent: "center",
         }}
       >
         <div
           style={{
-            height: "60px",
+            height: "100%",
             width: "100%",
-            backgroundColor: "white",
-            padding: "20px",
-            borderRadius: "10px 10px 0px 0px",
+            border: "solid 1px black",
             display: "flex",
-            justifyContent: "space-between",
+            justifyContent: "center",
             alignItems: "center",
           }}
         >
-          <p style={{ fontSize: "16px", fontWeight: "600" }}>
-            Uploaded Data Logs
-          </p>
-          {/* <Button onClick={onOpen} colorScheme="green">
-            Add New User
-          </Button> */}
-        </div>
-        <div style={{ height: "100%", width: "100%" }}>
-          //TbDeviceAnalytics
+          <div className={styles.card}>
+            <div className={styles.cardImage}>
+              <Image src={photo} alt="Logo" width={100} height={75} />
+            </div>
+            <div className={styles.cardText}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  gap: "40px",
+                  borderBottom: "solid 1px black",
+                  alignItems: "center",
+                  width: "400px",
+                }}
+              >
+                <p>5 jan 2024</p>
+                <p>catogory</p>
+                <p>name</p>
+              </div>
+
+              <h2 style={{fontSize: "20px", fontWeight: "600", marginTop: "10px"}}>Heading</h2>
+              <p>Lorem ipsum dolor seius officia ipsa reprehenderit minus, enim quis explicabo. Qui.</p>
+            </div>
+          </div>
         </div>
       </div>
-   
     </div>
   );
 };
