a7019d5012264bc48bbdbc1c7746228597565f58
afsar1ansel <mdafsaransal@gmail.com>
2025-02-13 14:18:56 +0530

    report

--- src/app/(dhashedpages)/Reports/page.tsx
+++ src/app/(dhashedpages)/Reports/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 import React from "react";
+import { NextPage } from "next";
 
-
-
-
-
-
-const Reports = () => {
+const Reports: NextPage = () => {
   return (
     <div style={{ width: "80vw", height: "60vh", maxWidth: "1250px" }}>
-    
+       Add some content here to prevent an empty page 
     </div>
   );
 };
